2add0a6a85de73370ef4aa2ae6a44988f433fb4c
Lucas Santos <leos.developer@gmail.com>
2023-06-24 20:17:46 -0300

    :rocket: add new config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,27 @@
+import { fontSans } from "@/config/fonts";
+import { projectConfig } from "@/config/project";
 import { Providers } from "@/contexts/providers";
 import clsx from "clsx";
+import { Metadata } from "next";
 import "./globals.css";
 
-// export const metadata: Metadata = {
-//   title: {
-//     default: projectConfig.name,
-//     template: `%s - ${projectConfig.name}`,
-//   },
-//   description: projectConfig.description,
-//   themeColor: [
-//     { media: "(prefers-color-scheme: light)", color: "white" },
-//     { media: "(prefers-color-scheme: dark)", color: "black" },
-//   ],
-// };
+export const metadata: Metadata = {
+  title: {
+    default: projectConfig.name,
+    template: `%s - ${projectConfig.name}`,
+  },
+  description: projectConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={clsx("w-screen h-screen bg-[#101014]")}>
+      <body className={clsx("h-screen bg-slate-800 dark:bg-[#101014]", fontSans.variable)}>
         <Providers>{children}</Providers>
       </body>
     </html>
