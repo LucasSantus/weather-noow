8cd264a2e817d3b15e8a812dbe02f1056924a9ba
Lucas Santos <leos.developer@gmail.com>
2023-06-27 00:21:23 -0300

    :wastebasket: remove files

--- src/config/project.ts
+++
@@ -1,9 +0,0 @@
-export type projectConfig = typeof projectConfig;
-
-export const projectConfig = {
-  name: "Weather Now",
-  description: "A simple application of weather",
-  links: {
-    github: "https://github.com/LucasSantus/weather-now",
-  },
-};
