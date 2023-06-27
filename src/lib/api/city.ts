eac55aa755bf8fe44ec71851866f67fdbcef1b33
Lucas Santos <leos.developer@gmail.com>
2023-06-27 00:21:24 -0300

    :wastebasket: remove files

--- src/lib/api/city.ts
+++
@@ -1,8 +0,0 @@
-import type { RawAxiosRequestConfig } from "axios";
-import { api } from "../axios";
-
-export class CityAPI {
-  static async getAll<T>(options?: RawAxiosRequestConfig): Promise<T> {
-    return await api.get<T>("geo/1.0/direct", options).then((response) => response.data ?? ([] as T));
-  }
-}
