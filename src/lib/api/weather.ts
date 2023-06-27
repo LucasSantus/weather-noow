afd4f4ea1bcd4f8804a3241cd9238e8653485c63
Lucas Santos <leos.developer@gmail.com>
2023-06-27 00:21:24 -0300

    :wastebasket: remove files

--- src/lib/api/weather.ts
+++
@@ -1,8 +0,0 @@
-import type { RawAxiosRequestConfig } from "axios";
-import { api } from "../axios";
-
-export class WeatherAPI {
-  static async get<T>(options?: RawAxiosRequestConfig): Promise<T> {
-    return await api.get("/data/2.5/weather", options);
-  }
-}
